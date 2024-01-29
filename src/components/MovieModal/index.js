71d95f9ff6ce43512a87a3e95375e9cfe752faa7
Willhong <hbilly2002@gmail.com>
2024-01-30 00:33:47 +0900

    row

---
+++ src/components/MovieModal/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const MovieModal = ({
+  backdrop_path,
+  title,
+  overview,
+  name,
+  release_date,
+  first_air_date,
+  vote_average,
+  setmodalOpen
+}) => {
+
+
+  return (
+    <div>MovieModal</div>
+  )
+}
+
+export default MovieModal
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
+import "./Row.css";
+import MovieModal from "./MovieModal";
 
-const Row = () => {
+const Row = ({ title, id, fetchUrl }) => {
+  const [movies, setmovies] = useState([]);
+  const [ModalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setmovieSelected] = useState({})
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
+
+  const fetchMovieData = async () => {
+    const response = await axios.get(fetchUrl);
+    setmovies(response.data.results);
+    console.log(response.data.results);
+  };
+  const handleClick = (movie) => {
+    console.log(movie);
+  }
   return (
-    <div>Row</div>
-  )
+    <div>
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow"
+          onClick={() => {
+            document.getElementById(id).scrollLeft -= window.innerWidth - 100;
+          }}
+          
+          >
+            {"<"}
+          </span>
+          </div>
+          <div id={id} className="row__posters">
+            {movies.map((movie) => (
+              <img
+                key={movie.id}
+                className="row__poster"
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt={movie.name}
+                onClick={() => handleClick(movie)}
+              />
+            ))}
+
+          </div>
+      <div className="slider__arrow-right">
+        <span className="arrow"
+        onClick={() => {
+          document.getElementById(id).scrollLeft += window.innerWidth - 100;
+        }}
+        >
+          {">"}
+        </span>
+        </div>
+      </div>
+        {setModalOpen && <MovieModal 
+        {...movieSelected}
+        setmodalOpen={setModalOpen}
+        />}
+        
+    </div>
+  );
 };
 
 export default Row;
+
